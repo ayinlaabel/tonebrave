6e70ae015571c786a71d1c52219fce5cf1bff5ad
ayinlaabel <kraneabel@gmail.com>
2019-08-13 02:36:41 +0100

    Error in Connecting to Mongodb

--- app.js
+++ app.js
@@ -2,22 +2,76 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 var logger = require('morgan');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
+
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Starting Database
+mongoose.connect('mongdb://localhost/tonebrave');
+
+
+let db = mongoose.connection;
+
+//Check for Errors and Startup
+db.once('open', () =>{
+  console.log('Mongo Start Successful on 27017...');
+});
+
+db.on('error', (err) => console.log(err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Connect-Flash Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+res.locals.messages = require('express-messages')(req, res);
+next();
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) =>{
+      var namespace = param.split('.'),
+          root = namespace.shift(),
+          formParam = root;
+      
+      while(namespace.length) {
+          formParam += '[' + namespace.shift() + ']';
+      }
+      return{
+          param : formParam,
+          msg : msg,
+          value : value
+      };
+  }
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,67 @@
 var express = require('express');
+const bcrypt = require('bcryptjs');
 var router = express.Router();
+
+//bring User Model
+let User = require('../models/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/register', (req, res)=>{
+  res.render('register');
+});
+
+router.post('/register', (req, res)=>{
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const passwordC = req.body.passwordC;
+
+  //Check for Validation
+  req.checkBody('name', 'Name is Required').notEmpty();
+  req.checkBody('email', 'Email is Required').notEmpty();
+  req.checkBody('email', 'Email should be like example@example.com').isEmail();
+  req.checkBody('username', 'Username is Required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('passwordC', 'The Password Dont Match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    console.log(errors);
+    res.render('register', {
+      errors:errors
+    });
+  }else{
+    let newUser = new User({
+      name:name,
+      email:email,
+      username:username,
+      password:password
+    });
+
+    bcrypt.genSalt(10, (err, salt) =>{
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save((err) => {
+          if (err) {
+            console.log(err);
+          }else{
+            req.flash('success', 'Registration Completed.');
+            res.redirect('/users/login');
+          }
+        })
+      })
+    })
+
+  }
 });
 
 module.exports = router;
