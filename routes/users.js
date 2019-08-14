03dab9cc47a0b5fbc2180f383d0cb83eba664395
ayinlaabel <kraneabel@gmail.com>
2019-08-14 21:32:14 +0100

    can register but cant Login Yet

--- routes/users.js
+++ routes/users.js
@@ -59,9 +59,12 @@ router.post('/register', (req, res)=>{
           }
         })
       })
-    })
-
+    });
   }
+});
+
+router.get('/login', (req, res) =>{
+  res.render('login');
 });
 
 module.exports = router;
